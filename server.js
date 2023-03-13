19c6f7af04ad6a826738678c84be3d0c7a0ae793
excellence-arch <oladipupomichael9@gmail.com>
2023-03-13 12:20:39 +0100

    feat: username to profile, logout & wildcard route

--- server.js
+++ server.js
@@ -78,7 +78,16 @@ myDB(async (client) => {
   );
 
   app.route("/profile").get(ensureAuthenticated, (req, res) => {
-    res.render("profile");
+    res.render("profile", { username: req.user.username });
+  });
+
+  app.route("/logout").get((req, res) => {
+    req.logout();
+    res.redirect("/");
+  });
+
+  app.use((req, res, next) => {
+    res.status(404).type("text").send("Not Found");
   });
 }).catch((e) => {
   console.log("Connection Unsuccessful");
